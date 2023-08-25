45fe7225d3695fb2e934ea86393b6978f66f1925
FRANKLINJOE019 <ekenzefranklinejoe2020@gmail.com>
2023-08-25 16:41:22 +0100

    reading inputs in java

---
+++ READ_INPUT/ReadAge.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class ReadAge{
+	public static void main(String[] args){
+	System.out.println("pls Enter your current age");
+Scanner input = new Scanner(System.in);
+System.out.println("your current age is " + input.nextInt());}}
+
