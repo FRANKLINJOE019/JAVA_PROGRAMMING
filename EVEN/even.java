3c772366d71bb94bc5f6e2a2440ef5b4216466fe
FRANKLINJOE019 <ekenzefranklinejoe2020@gmail.com>
2023-10-05 14:36:01 +0100

    printing even numbers

---
+++ EVEN/even.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class even {
+
+  public static void main(String[] args) {
+
+int i;
+for(i=1;i>=1 && i<=100;i++){
+   if(i%2==0)
+System.out.print(i + " ");
+
+}}}
